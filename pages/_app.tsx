b092edd539d9f102fe644ce97cd4f922722c3eaa
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-06-06 14:06:07 +0500

    options

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { FunctionComponent } from 'react';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
