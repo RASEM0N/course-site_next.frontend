132ef7a8ee5b90df2887e14dde775531c681cc2d
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-06-06 18:41:09 +0500

    ...

--- components/Text/Text.tsx
+++ components/Text/Text.tsx
@@ -1,20 +1,23 @@
-import React, { FunctionComponent, ReactNode } from 'react'
+import React, { DetailedHTMLProps, FunctionComponent, HTMLAttributes, ReactNode } from 'react'
 import styles from './Text.module.css'
 import cName from 'classnames'
 
-interface PropsType {
+type PHTMLProps = DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>
+
+interface PropsType extends PHTMLProps {
     size?: 'small' | 'large' | 'medium'
     children: ReactNode
 }
 
-export const Text: FunctionComponent<PropsType> = ({ size, children }) => {
+export const Text: FunctionComponent<PropsType> = ({ size, children, className, ...props }) => {
     return (
         <p
-            className={cName(styles.text, {
+            className={cName(styles.text, className, {
                 [styles.small]: size === 'small',
                 [styles.medium]: size === 'medium',
                 [styles.large]: size === 'large',
             })}
+            {...props}
         >
             {children}
         </p>
