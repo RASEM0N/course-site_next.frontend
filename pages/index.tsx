f906e1c582933bbbccbbc09fc290ef3216ed5f30
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-06-06 15:42:22 +0500

    create: custom h element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import { FunctionComponent } from 'react'
+import { Htag } from '../components/Htag/Htag'
 
 const Home: FunctionComponent = () => {
-    return <div>Text</div>
+    return (
+        <div>
+            <Htag type="h1">Text</Htag>
+        </div>
+    )
 }
 
 export default Home
